c68b14329ed33270d6e33334b51c9e5164cd4c2d
sonikaraina11 <2020a1r12mietjammu.in>
2021-03-20 09:25:59 -0700

    Armstrong program

---
+++ SumLoop.c
@@ -0,0 +1,21 @@
+#include<stdio.h> //Pre-process directive to include standard input and output functions header file
+
+ int main(){          //Main function
+     int number,lastDigit,sum=0;   // variable declaration with datatype int
+     
+     //printf and scanf function calling
+     printf("Enter any number: ");   
+     scanf("%d",&number);
+        
+        // Whileloop to calculate the sum
+     while(number!=0)           
+     {
+         lastDigit=number%10;
+         sum += lastDigit;
+         number/=10;
+     }
+
+     printf("Sum of digits =  %d\n",sum);  //print the sum of the numbers assigned by the user
+     return 0;      // return function
+ }
+© 2021 G
