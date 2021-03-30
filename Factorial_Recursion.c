edb340f7d369eddb45894c4c808d4fd90d7d6ab6
sonikaraina11 <2020a1r12mietjammu.in>
2021-03-30 21:24:24 +0530

    Attendence Program

---
+++ Factorial_Recursion.c
@@ -0,0 +1,19 @@
+
+// Purpose:To Check whether the number is Armstrong or not  
+#nclude<stdio.h> // Pre-Processive to include standard input and output funtions header file
+long int multiplyNumbers(int n); // Recursion
+int main() { // Start of Body 
+    int n; // Variable  Declaration
+    printf("Enter a positive integer: "); // taking Input
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n)); // Printing Output 
+    return 0;
+}
+
+long int multiplyNumbers(int n) { 
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
+// End of Body 
