52db7fb67105e70d956f9fe7bbeed28a50a97161
sonikaraina11 <2020a1r12mietjammu.in>
2021-04-04 11:49:28 +0530

    03_Hello program

---
+++ 03_Hello.c
@@ -0,0 +1,8 @@
+//A basic C Program to print Hello World.
+//Start
+#include<stdio.h> //Including standard input ouput header files from library.
+int main(){ 
+	printf("Hello Buddy!!");//Here we entered the texted to be printed.
+	return 0;//Returning.
+} 
+//End
